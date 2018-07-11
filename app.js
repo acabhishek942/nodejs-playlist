2226fa986749c60e04c4e4ac3596044b84365e3b
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-11 23:03:01 +0530

    event emitter using traditional and ES6 ways

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
-var stuff = require('./stuff');
+var events = require('events');
+var utils = require('util');
 
-console.log(stuff.counter([1, 2, 3]));
-console.log(stuff.adder(1,2));
-console.log(stuff.pi);
+var Person = function(name){
+  this.name = name;
+}
+
+class PersonClass extends events.EventEmitter {
+  constructor(name){
+    super();
+    this.name = name;
+  }
+}
+
+utils.inherits(Person, events.EventEmitter);
+
+var james = new Person('James');
+var mary = new Person('Mary');
+var ryu = new Person('Ryu');
+
+var jamesES6 = new PersonClass('James');
+var maryES6 = new PersonClass('Mary');
+var ryuES6 = new PersonClass('Ryu');
+
+var people = [james, mary, ryu]
+
+//ES6 style
+var peopleES6 = [jamesES6, maryES6, ryuES6]
+
+people.forEach(function(person){
+  person.on('speak', function(msg){
+    console.log(person.name + ' said ' + msg);
+  });
+});
+
+peopleES6.forEach(function(personES6){
+  personES6.on('speakES6', function(msg){
+    console.log(personES6.name + ' said ' + msg);
+  });
+});
+
+james.emit('speak', 'Hey dudes');
+jamesES6.emit('speakES6', 'ES6 is supposed to be better');
