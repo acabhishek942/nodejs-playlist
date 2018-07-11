8dcc801a56a28fde0b3926ae80d03bc50f9a529f
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-12 03:22:02 +0530

    readable streams

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-  console.log('req was made' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hey Ninjas');
+var myReadStream = fs.createReadStream(__dirname + '/writee.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+  console.log('New chunk received');
+  console.log(chunk);
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('300 port open');
+// var server = http.createServer(function(req, res){
+//   console.log('req was made' + req.url);
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hey Ninjas');
+// });
+//
+// server.listen(3000, '127.0.0.1');
+// console.log('300 port open');
