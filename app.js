06aff759eae9a9680f220eb832026a683ce34a06
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-13 22:16:29 +0530

    writeable Streams

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/writee.txt', 'utf8');
 
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
+
 myReadStream.on('data', function(chunk){
   console.log('New chunk received');
-  console.log(chunk);
+  myWriteStream.write(chunk);
 });
 
 // var server = http.createServer(function(req, res){
