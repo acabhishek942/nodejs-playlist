355517360e1837c119eb4a1ab0777bfd3b293f18
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-14 15:17:46 +0530

    sending json response

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
   console.log('req was made' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  myReadStream.pipe(res)
+  res.writeHead(200, {'Content-Type': 'application.json'});
+  var myObjc = {
+    'name': 'Ryu',
+    'job': 'Ninja',
+    'age': 29
+  }
+  res.end(JSON.stringify(myObjc));
 });
 
 server.listen(8080, '127.0.0.1');
