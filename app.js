ba34ceaffd83889ef3a8015cc4d5f260f402cc39
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-14 22:03:59 +0530

    getting started with express

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
+var app = express();
 
-
-var server = http.createServer(function(req, res){
-  console.log('req was made' + req.url);
-  if (req.url === '/home' || req.url === '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if (req.url === '/contact-us') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  } else if (req.url==='/api/ninjas') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var ninjas = [
-      {'name': 'Ryu', 'age': 29},
-      {'name': 'Yoshi', 'age': 32},
-    ]
-    res.end(JSON.stringify(ninjas));
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
+app.get('/', function(req, res){
+  res.send("This is the homepage");
 });
 
-server.listen(8080, '127.0.0.1');
-console.log('8080 port open');
+app.get('/contact', function(req, res){
+  res.send("This is the conatc page");
+});
+
+
+app.listen(3000);
