b528ac509aab85e71cb1be5f3f7f89a6720fa030
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-14 22:29:02 +0530

    just starting to use ejs templating engine

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send("This is the homepage");
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-  res.send("This is the conatc page");
+  res.sendFile(__dirname + '/contact.html');
 });
 
-app.get('/profile/:id', function(req, res){
-  res.send('Youu requested id ' + req.params.id);
+app.get('/profile/:name', function(req, res){
+  var data = {
+    'age': 29,
+    'job': 'Ninja'
+  }
+  res.render('profile', {person: req.params.name, data: data});
 })
 
 
