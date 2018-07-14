22902350e3d7087cd160376d8f4343a4eb70648e
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-14 13:27:28 +0530

    writing and serving text contents using pipe

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/writee.txt', 'utf8');
 
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt')
 
-myReadStream.on('data', function(chunk){
-  console.log('New chunk received');
-  myWriteStream.write(chunk);
+var server = http.createServer(function(req, res){
+  console.log('req was made' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var myReadStream = fs.createReadStream(__dirname + '/writee.txt');
+  myReadStream.pipe(res)
 });
 
-// var server = http.createServer(function(req, res){
-//   console.log('req was made' + req.url);
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hey Ninjas');
-// });
-//
-// server.listen(3000, '127.0.0.1');
-// console.log('300 port open');
+server.listen(3000, '127.0.0.1');
+console.log('3000 port open');
