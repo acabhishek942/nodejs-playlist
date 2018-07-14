09fe4a18b72b657fc6711f0fd83f1971454c9284
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-14 15:12:04 +0530

    rendering html

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
   console.log('req was made' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  var myReadStream = fs.createReadStream(__dirname + '/writee.txt');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
   myReadStream.pipe(res)
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('3000 port open');
+server.listen(8080, '127.0.0.1');
+console.log('8080 port open');
