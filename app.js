56d76023b1b589ce9b4b23cc74ee405e5225a62f
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-14 22:11:01 +0530

    route parameters

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ app.get('/contact', function(req, res){
   res.send("This is the conatc page");
 });
 
+app.get('/profile/:id', function(req, res){
+  res.send('Youu requested id ' + req.params.id);
+})
+
 
 app.listen(3000);
