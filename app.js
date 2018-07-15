00a87fcc2471cb1d852de730e2fff8dacda643e1
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-15 14:11:24 +0530

    handling post requests

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'))
@@ -10,8 +14,12 @@ app.get('/', function(req, res){
 });
 
 app.get('/contact', function(req, res){
-  console.log(req.query);
   res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req, res){
+  console.log(req.body);
+  res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', function(req, res){
