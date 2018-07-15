3ea6ed27fdb3d387e13decb6a6f0cd711f4d78ff
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-15 23:32:21 +0530

    hooking up to mlab db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,20 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to mlab DB
+mongoose.connect('mongodb://test:testing1@ds137957.mlab.com:37957/acabhishek42_todo')
+
+// create schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+  item: String
+})
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+var item1 = Todo({item: 'Buy flowers'}).save(function(err){
+  if (err) throw err;
+  console.log('Item saved');
+});
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
